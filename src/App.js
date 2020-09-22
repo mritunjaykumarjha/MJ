e3ad0af035c99434ed835cac1f92f37066744278
Mritunjay Kumar jha <jhamrk@gmail.com>
2020-09-23 02:30:37 +0530

    initial1.1.q

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {BrowserRouter as Router,} from 'react-router-dom' 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -9,12 +8,12 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <div>
       <Header />
       <Home />
-      
       <Footer />
-    </Router>
+    </div>
+      
     
   );
 }
