de39839fd9102ed6cb1a92dfe9656dca05ccb929
Mritunjay Kumar jha <jhamrk@gmail.com>
2020-09-23 02:13:23 +0530

    Updates

---
+++ precache-manifest.7c4ffe3288830d0ee090f1be782c6c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742eec86ad8c22820d841d48a73a4872",
+    "url": "/MJ/index.html"
+  },
+  {
+    "revision": "8ee0c52652ccef0a32a1",
+    "url": "/MJ/static/css/main.0e6f1d77.chunk.css"
+  },
+  {
+    "revision": "e57ea31514b23d9df33c",
+    "url": "/MJ/static/js/2.5f0fef93.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MJ/static/js/2.5f0fef93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee0c52652ccef0a32a1",
+    "url": "/MJ/static/js/main.a68f316e.chunk.js"
+  },
+  {
+    "revision": "9cdc868d4de76bb86be1",
+    "url": "/MJ/static/js/runtime-main.dceb20a8.js"
+  }
+]);
