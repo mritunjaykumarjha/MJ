1f0d76307598c08924a4b38d8945a55313d20120
Mritunjay Kumar jha <jhamrk@gmail.com>
2020-09-23 02:54:36 +0530

    initial1.12

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom' 
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import About from './components/About';
-import Intro from './components/Intro';
-import Skills from './components/Skills';
-import Work from './components/Work';
-
 import './App.css';
 
 function App() {
@@ -15,13 +10,10 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
-      <Route  path="/" component={Home} />
-        <Route exact path="/About" component={About} />
-        <Route exact path="/Intro" component={Intro} />
-        <Route exact path="/Skills" component={Skills} />
-        <Route exact path="/Work" component={Work} />
-      </Switch>
+        <Route exact path="/" component={Home} />
       
+      
+      </Switch>
       <Footer />
     </Router>
     
