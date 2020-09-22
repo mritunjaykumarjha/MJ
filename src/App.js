db8edac340414e9a869a018fb7dbbb3ee33b62e6
Mritunjay Kumar jha <jhamrk@gmail.com>
2020-09-23 02:28:50 +0530

    initial1.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom' 
+import {BrowserRouter as Router,} from 'react-router-dom' 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import About from './components/About';
+
+
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Switch>
-      <Route exact path="/" component={Home} />
-        <Route exact path="/About" component={About} />
-      </Switch>
+      <Home />
       
       <Footer />
     </Router>
