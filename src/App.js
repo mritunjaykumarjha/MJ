e7dcc4fbc4738f0a979aac3bacadc2a4635cdc75
Mritunjay Kumar jha <jhamrk@gmail.com>
2020-09-23 02:49:14 +0530

    initial1.10q

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom' 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-
+import About from './components/About';
+import Intro from './components/Intro';
+import Skills from './components/Skills';
+import Work from './components/Work';
 
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
-      <Home />
-      <Footer />
-    </div>
+      <Switch>
+      <Route  path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/Intro" component={Intro} />
+        <Route exact path="/Skills" component={Skills} />
+        <Route exact path="/Work" component={Work} />
+      </Switch>
       
+      <Footer />
+    </Router>
     
   );
 }
